9d5c288dbd597534e825650208c40a0f15da155c
Jiangyi Lin <lin.jian@husky.neu.edu>
2018-05-29 22:01:25 +0800

    register

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
     re_path(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
     re_path(r'^add_category/$', views.add_category, name='add_category'),
+    re_path(r'^register/$', views.register, name='register'),
 ]
